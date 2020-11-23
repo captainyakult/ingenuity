a2d0874069c9d7bb840ec9efa0cc37e20a73da34
Stephen Hurley <hurley@jpl.nasa.gov>
2020-11-23 09:36:47 -0800

    #463 Added deploy script.

---
+++ build.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Populates the build folder for deployment.
+
+set -e
+
+if [[ ($# -ne 3) || (($1 != "dev") && ($1 != "staging") && ($1 != "production")) ]] ; then
+	echo "usage: build.sh <dev|staging|production> <version> <build folder>"
+	exit 1
+fi
+
+QA_LEVEL=$1
+VERSION=$2
+BUILD_FOLDER=$3
+
+# Get git branch
+PIONEER_BRANCH=$QA_LEVEL
+if [[($QA_LEVEL = "dev")]]; then
+	PIONEER_BRANCH="mars2020"
+fi
+if [[($QA_LEVEL = "staging")]]; then
+	PIONEER_BRANCH="mars2020"
+fi
+if [[($QA_LEVEL = "production")]]; then
+	PIONEER_BRANCH="mars2020"
+fi
+
+# Update the pioneer-js.
+echo "Making sure pioneer-js is up-to-date."
+pushd ../pioneer-js > /dev/null
+git fetch -p
+git checkout $PIONEER_BRANCH
+git reset --hard origin/$PIONEER_BRANCH
+git clean -dfx
+yarn setup
+popd > /dev/null
+
+# Update pioneer-scripts
+echo "Making sure pioneer-scripts is up-to-date."
+pushd ../pioneer-scripts > /dev/null
+git fetch -p
+git checkout $PIONEER_BRANCH
+git reset --hard origin/$PIONEER_BRANCH
+git clean -dfx
+yarn setup
+popd > /dev/null
+
+# Update the dependencies.
+yarn setup
+
+# Build the app
+if [[($QA_LEVEL = "staging") || ($QA_LEVEL = "production")]]; then
+	yarn build
+else
+	yarn build-dev
+fi
+
+# Create the config.js
+if [[($QA_LEVEL = "production")]]; then
+	printf "config = {\n\tstaticAssetsUrl: 'https://eyes.nasa.gov/assets/static',\n\tdynamicAssetsUrl: 'https://eyes.nasa.gov/assets/dynamic',\n}\n" > dist/config.js
+elif [[($QA_LEVEL = "staging")]]; then
+	printf "config = {\n\tstaticAssetsUrl: 'https://eyes.nasa.gov/staging/assets/static',\n\tdynamicAssetsUrl: 'https://eyes.nasa.gov/staging/assets/dynamic',\n}\n" > dist/config.js
+else
+	printf "config = {\n\tstaticAssetsUrl: 'https://eyes.nasa.gov/dev/assets/static',\n\tdynamicAssetsUrl: 'https://eyes.nasa.gov/dev/assets/dynamic',\n}\n" > dist/config.js
+fi
+
+# Moves files into the build folder.
+rsync -rtvz --delete dist/ "$BUILD_FOLDER/"
+
