e1e2c3ec7d42690b5db43d9d273d73c4fef18d82
Jack Simpson <jsimpson.3d@gmail.com>
2023-06-09 20:48:36 +0000

    Update 25 files

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,7 @@ import "./styles.css";
 useTexture.preload("/textures/colormap_4196x2048_q75.jpg");
 useTexture.preload("/textures/heightmap_4196x2048_q85.jpg");
 useTexture.preload("/textures/normalmap_4196x2048_q95_-1to1.jpg");
+useGLTF.preload("/models/ingenuity/ingenuity.gltf");
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
